94c7956de45804bbf9a180407264e8640d929058
Tino Göhlert <tino.gohlert@lovoo.com>
2024-04-29 00:10:01 +0200

    fix namespace names

--- Editor/MapImporter/SolidImporter.cs
+++ Editor/MapImporter/SolidImporter.cs
@@ -1,11 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Qunity;
 using UnityEditor;
 using UnityEngine;
 
-namespace Quinity
+namespace Qunity
 {
     [System.Serializable]
     public class SolidImporter : BaseEntityImporter
